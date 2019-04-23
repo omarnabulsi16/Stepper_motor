d64d77c3f7f84cca1c06f38b234c6f82d8eeff29
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2019-04-23 14:42:14 -0700

    Update Haversine.py
    added comments and math formula

--- Haversine.py
+++ Haversine.py
@@ -1,5 +1,7 @@
+#libraries
 import math
 
+#distance function
 def setDistance(source, destination):
         '''
         Description:
@@ -10,10 +12,16 @@ def setDistance(source, destination):
         Returns:
             Nothing
         '''
+        
+        #initialization of destination and initial and final coordinates
         a1, b1 = source
         a2, b2 = destination
         
+        #radius of Earth
         radius = 6371 # km
-
+        
+        #math formulas to calculate the distance between 2 coordinates
         da = math.radians(a2-a1)
         db = math.radians(b2-b1)
+        a = math.sin(da/2) * math.sin(da/2) + math.cos(math.radians(a1)) \ 
+                * math.cos(math.radians(a2)) * math.sin(db/2) * math.sin(db/2)
